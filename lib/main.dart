4b8aefdae3717aa24184578587c55c3f7327002f
Hossein Ronaghi <hosseinronaghi@gmail.com>
2026-09-18 00:41:33 +0330

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'app/app.dart';
+import 'core/migration/primary_data_migration.dart';
+import 'core/migration/safe_upgrade.dart';
 import 'core/notifications/reminder_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  GoogleFonts.config.allowRuntimeFetching = true;
+  await SafeUpgradeCoordinator.prepare();
+  await PrimaryDataMigrationCoordinator.migrateAll();
   await ReminderService.instance.initialize();
   runApp(const ProviderScope(child: RahaLifeApp()));
 }
